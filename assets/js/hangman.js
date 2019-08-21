e52c156b3a8fc682454567392af15cfe65eef9d8
Jonathan Roth <rothje@vcu.edu>
2019-08-21 19:47:32 -0400

    added basic html and js file with pseudo code

---
+++ assets/js/hangman.js
@@ -0,0 +1,51 @@
+// Game Structure
+// -----------------------
+// 1. Words/Letters
+// 2. Player Guesses
+// 3. Guesses = true/false
+// 4. Keep track of player input 
+// 5. Show player Progress
+// 6. Finish game with win or loss
+
+// Declare variables, functions, and loops
+// ---------------------------
+
+// Array of Words
+
+alert("dude");
+
+
+var words = [
+    "pasta",
+    "donkey",
+    "structure",
+    "pizza",
+    "ranger",
+    "goodness",
+    "silhouette",
+    "blowtorch",
+    "bandwagon",
+    "perspicacity",
+    "loquacious",
+    "elocution",
+    "vistas"];
+
+
+
+
+// Set Random word
+
+var randomWord = words[Math.floor(Math.random() * words.length)];
+
+// var wordSplit = elementInArray.split(" "); Pseudocode
+
+// var answerArray = "_" Pseudocode
+
+for (var i = 0; i < words.length; i++) {
+    answerArray([i])
+}
+
+
+
+
+
